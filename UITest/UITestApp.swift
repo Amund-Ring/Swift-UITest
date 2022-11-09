09547c6b295afa8cac84ccd199c1f685d6a4a151
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-11-09 11:00:42 +0100

    Initial Commit

---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Amund Ring on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
